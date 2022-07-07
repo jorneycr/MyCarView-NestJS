ea31a3137a2ad39b49a971faa9e6eb0b9a470814
jorneylopez <jorney95@live.com>
2022-07-06 22:25:27 -0600

    creating end-points in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -11,5 +12,25 @@ export class UsersController {
     createUser(@Body() createUserDto: CreateUserDto) {
         const { email, password } = createUserDto;
         this.usersService.create(email, password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), updateUserDto);
     }
 }
