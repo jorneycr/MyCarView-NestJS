4d390001da7abccc4e4b6050c478aec3c84e6cbd
jorneylopez <jorney95@live.com>
2022-07-16 14:47:16 -0600

    adding unit testing users

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,41 @@
+import { User } from './user.entity';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({ id, email: 'jl@live.com', password: 'test' } as User)
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'test' } as User])
+      },
+      // remove: (id:number) => { },
+      // update: () => { },
+    };
+    fakeAuthService = {
+      // signup: () => { },
+      // signin: () => { },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService
+        }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
