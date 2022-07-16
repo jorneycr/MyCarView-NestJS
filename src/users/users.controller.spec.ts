515c16f5794c936122027a3d1f602afb0509962c
jorneylopez <jorney95@live.com>
2022-07-16 15:42:42 -0600

    adding unit testing users

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -22,7 +23,9 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => { },
-      // signin: () => { },
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User)
+      },
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -44,4 +47,34 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('jl@live.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('jl@live.com');
+  });
+
+  it('findUser  returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    try {
+      await controller.findUser('1');
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotFoundException);
+      expect(err.message).toBe('User not found');
+    }
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin({ email: 'jl@live.com', password: 'test' }, session);
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1)
+  });
+
 });
