65d4bfc368f1762946bb25655e49fc0d22435548
jorneylopez <jorney95@live.com>
2022-07-17 21:59:59 -0600

    add some structure reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportsService: ReportsService) { }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() createReportDto: CreateReportDto) {
+        this.reportsService.create(createReportDto);
+    }
+}
